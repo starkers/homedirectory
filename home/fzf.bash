be00ab9178c97404f1c942d095589a63ca9c1946
David Stark <david@starkers.org>
2018-10-11 22:16:44 +0100

    fonts and fzf now from AUR and no longer tracked in git

--- home/fzf.bash
+++ home/fzf.bash
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
-  export PATH="$PATH:${HOME}/.fzf/bin"
+if [[ ! "$PATH" == */home/starkers/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/starkers/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/share/fzf/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/starkers/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/share/fzf/key-bindings.bash"
+source "/home/starkers/.fzf/shell/key-bindings.bash"
 
--- home/fzf.zsh
+++ home/fzf.zsh
@@ -1,14 +1,14 @@
-# # Setup fzf
-# # ---------
-# if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
-#   export PATH="$PATH:${HOME}/.fzf/bin"
-# fi
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/starkers/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/starkers/.fzf/bin"
+fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/share/fzf/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/starkers/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/share/fzf/key-bindings.zsh"
+source "/home/starkers/.fzf/shell/key-bindings.zsh"
 
