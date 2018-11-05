503eed29843fdf4af3f84b34f6ebc1462c68a92e
David Stark <david@starkers.org>
2018-11-05 11:00:33 +0000

    ensure some gnome things I like are done automatically

---
+++ tweaks/common/50-gnome.sh
@@ -0,0 +1,3 @@
+gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier '<Alt>'
+gsettings set org.gnome.desktop.interface clock-show-seconds true
