7ec580bda83a2f454c0c89dd9389b3f90759ff81
David Stark <david@starkers.org>
2020-02-27 17:13:17 +0000

    stuuuuuf

--- home/screenlayout/work-dual.sh
+++ home/screenlayout/work-dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-1 --off --output eDP-1 --off --output DP-2 --mode 1920x1200 --pos 0x0 --rotate normal
+xrandr --output eDP1 --mode 2048x1152 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --primary --mode 2560x1440 --pos 2048x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
