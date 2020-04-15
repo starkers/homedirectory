a9158443f296c93e88291c3cd47f241f4e452cd3
David Stark <david@starkers.org>
2020-04-15 14:18:04 +0100

    matebook 1920

--- home/screenlayout/home-external-max.sh
+++ home/screenlayout/home-external-max.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI-1 --mode 3440x1440 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --off --output DP-2 --off
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP2-1 --primary --mode 3440x1440 --pos 0x0 --rotate normal --output DP2-2 --off --output DP2-3 --off --output HDMI1 --off --output VIRTUAL1 --off
