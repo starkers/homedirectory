11c88cedeffc97ef3997f5e9b4ff24e546b5782b
David Stark <david@starkers.org>
2020-09-28 21:18:10 +0100

    meh

--- home/polybar/kube-context.sh
+++ home/polybar/kube-context.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 
-/home/starkers/.software/bin/kubectl config current-context
+FOO="$(/home/starkers/.software/bin/kubectl config current-context 2>&1)"
+
+RC="$?"
+
+if [ ! "X${RC}" == X0 ]; then
+  echo kube: no cluster
+else
+  echo kube: ${FOO}
+fi
 
---
+++ home/polybar/launch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pkill -9 polybar
+
+# polybar -r bump &
+polybar -r main &
